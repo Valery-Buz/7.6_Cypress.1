0cba943320ca456fd6caec434a57872266f0e3d5
Valery Buz <Ajven@yandex.ru>
2023-02-18 01:05:49 +0300

    Initial commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,70 @@
+describe("Page authorization test", () => {
+  beforeEach(() => {
+    cy.visit("/booksNode");
+  });
+
+  it("Should be visible", () => {
+    cy.contains("Books list").should("be.visible");
+    cy.get("a > span > span").should("have.class", "ml-2");
+  });
+  it("Should successfully login", () => {
+    cy.login("bropet@mail.ru", "123");
+    cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
+  });
+
+  it("Should not login with empty login", () => {
+    cy.contains("Log in").click();
+    cy.get("#mail").type(" ");
+    cy.get("#pass").type("test");
+    cy.contains("Submit").click();
+    cy.get("#mail")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+    cy.get("#mail")
+      .then(($el) => $el[0].validationMessage)
+      .should("contain", "Заполните это поле.");
+  });
+
+  it("Should not login with empty password", () => {
+    cy.contains("Log in").click();
+    cy.get("#mail").type("test@test.com");
+    cy.contains("Submit").click();
+    cy.get("#pass")
+      .then(($el) => $el[0].checkValidity())
+      .should("be.false");
+  });
+});
+
+describe("Testing favorite book list", () => {
+  beforeEach(() => {
+    cy.visit("/booksNode");
+    cy.login("bropet@mail.ru","123");
+  });
+
+  it("Add new favorite book",() => {
+    cy.contains("Books list").click();
+    cy.contains("Add new").click();
+    cy.typing("#title", "Идиот");
+    cy.typing("#description.form-control",
+    "Роман Ф.М.Достоевского.  Впервые был опубликован в номерах журнала «Русский вестник» за 1868 год."
+  );
+    cy.typing("#authors", "Фёдор Михайлович Достоевский");
+    cy.get("#favorite").click();
+    cy.get("form > .ml-2").click();
+    cy.get("h4").click();
+    cy.contains(".card-text", "Фёдор Михайлович Достоевский").should("be.visible");
+});
+
+it("Should delete from favorite", () => {
+  cy.get("h4").click();
+  cy.get(".card-footer > .btn").click();
+  cy.contains('.btn > a', "Please add some book to favorit on home page!").should("be.visible");
+});
+
+it("Should add book to favorite from BookList", () => {
+  cy.contains("Books list").click();
+  cy.contains("Add to favorite").click();
+  cy.get("h4").click();
+  cy.contains(".card-text", "Фёдор Михайлович Достоевский").should("be.visible");
+});
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,60 @@
+Cypress.Commands.add("login", (login, password) => {
+    cy.contains("Log in").click();
+    cy.get("#mail").type(login);
+    cy.get("#pass").type(password);
+    cy.contains("Submit").click();
+  });
+  
+  Cypress.Commands.add("loginOnly", (login) => {
+    cy.contains("Log in").click();
+    cy.get("#mail").type(login);
+    cy.contains("Submit").click();
+  });
+  
+  Cypress.Commands.add("typing", (selector, text) => {
+    cy.get(selector).type(text);
+  });
+  
+  Cypress.Commands.add('addBook', (title, description, author, favorite) => {
+    cy.contains('Add new').click();
+    cy.get('#title').type(title);
+    cy.get('#description').type(description);
+    cy.get('#authors').type(author);
+    if (favorite) {
+        cy.get('#favorite').click();
+    }
+    cy.contains('Submit').click();
+});
+
+Cypress.Commands.add('favorite', (title) => {
+    
+    cy.contains('Favorites').click();
+
+    cy.contains(title).should('be.visible');
+});
+
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
